24a2743215fcaf82dda7720f25d721a4027296d3
Caio Tácito Borges da Costa <caio.tacito@protonmail.com>
2021-05-04 14:12:39 -0300

    modificar - códigos de exemplo

--- source/src/temperatura-ar.cpp
+++ source/src/temperatura-ar.cpp
@@ -1,10 +1,10 @@
 #include <OneWire.h> //INCLUSÃO DE BIBLIOTECA
-  #include <DallasTemperature.h> //INCLUSÃO DE BIBLIOTECA
+#include <DallasTemperature.h> //INCLUSÃO DE BIBLIOTECA
 
-  #define DS18B20 7 //DEFINE O PINO DIGITAL UTILIZADO PELO SENSOR
+#define DS18B20 7 //DEFINE O PINO DIGITAL UTILIZADO PELO SENSOR
 
-  OneWire ourWire(DS18B20); //CONFIGURA UMA INSTÂNCIA ONEWIRE PARA SE COMUNICAR COM O SENSOR
-  DallasTemperature sensors(&ourWire); //BIBLIOTECA DallasTemperature UTILIZA A OneWire
+OneWire ourWire(DS18B20); //CONFIGURA UMA INSTÂNCIA ONEWIRE PARA SE COMUNICAR COM O SENSOR
+DallasTemperature sensors(&ourWire); //BIBLIOTECA DallasTemperature UTILIZA A OneWire
 
   void setup()
   {
@@ -20,4 +20,4 @@
     Serial.print(sensors.getTempCByIndex(0)); //IMPRIME NA SERIAL O VALOR DE TEMPERATURA MEDIDO
     Serial.println("*C"); //IMPRIME O TEXTO NA SERIAL
     delay(250);//INTERVALO DE 250 MILISSEGUNDOS
-  } 
+  }
